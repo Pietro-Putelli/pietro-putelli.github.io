00f8937a6116e0bd7dd1c7504258e3a0d05f7867
pietroputelli@gmail.com <pietroputelli@gmail.com>
2022-09-25 03:48:11 +0200

    add title

--- src/App.js
+++ src/App.js
@@ -3,17 +3,21 @@ import { About, Contacts, Home, Projects } from "./pages";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
+  document.title = "Pietro Putelli";
+
   return (
-    <BrowserRouter
-    // basename="https://pietroputelli.com"
-    >
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contacts" element={<Contacts />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <BrowserRouter
+      // basename="https://pietroputelli.com"
+      >
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contacts" element={<Contacts />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
